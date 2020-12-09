0e78795f5dc422d0152e99f81b534fd9739411bf
Juan Antonio Beato <juan.beato44@gmail.com>
2020-12-09 16:00:42 +0100

    feat(slot): added right and left slots and styles

--- packages/jb-header/src/JbHeader.js
+++ packages/jb-header/src/JbHeader.js
@@ -1,4 +1,6 @@
 import { html, css, LitElement, unsafeCSS } from 'lit-element';
+import '@polymer/iron-icon/iron-icon.js';
+import '@polymer/iron-icons/iron-icons.js';
 import style from './JbHeader.scss';
 
 export class JbHeader extends LitElement {
@@ -12,25 +14,52 @@ export class JbHeader extends LitElement {
 
   static get properties() {
     return {
-      title: { type: String },
-      counter: { type: Number },
+      leftText: { type: String },
+      leftIcon: { type: String },
+      rightText: { type: String },
+      rightIcon: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.title = 'Hey there';
-    this.counter = 5;
-  }
-
-  __increment() {
-    this.counter += 1;
+    this.leftText = '';
+    this.leftIcon = '';
+    this.rightText = '';
+    this.rightIcon = '';
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <div class="jb-header">
+        ${this._containerTemplate('left')} ${this._containerTemplate('right')}
+      </div>
     `;
+  }
+
+  _containerTemplate(type) {
+    return html`
+      <div class="jb-header__${type}-container">
+        <slot name=${type}>
+          ${this.constructor._itemTextTemplate(this[`${type}Text`], type)}
+          ${this.constructor._itemIconTemplate(this[`${type}Icon`], type)}
+        </slot>
+      </div>
+    `;
+  }
+
+  static _itemIconTemplate(icon, type) {
+    return icon
+      ? html`<iron-icon
+          class="jb-header__${type}-container__icon"
+          icon=${icon}
+        ></iron-icon>`
+      : html``;
+  }
+
+  static _itemTextTemplate(text, type) {
+    return text
+      ? html`<p class="jb-header__${type}-container__text">${text}</p>`
+      : html``;
   }
 }
