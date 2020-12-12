45dc73f6545b04c80bec823d5e8cbe379ea9e131
Juan Antonio Beato <juan.beato44@gmail.com>
2020-12-12 19:50:24 +0100

    feat(jb-product-card component): added skeleton

--- packages/jb-product-card/src/JbProductCard.js
+++ packages/jb-product-card/src/JbProductCard.js
@@ -1,6 +1,8 @@
 import { html, css, LitElement, unsafeCSS } from 'lit-element';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html';
 import '@polymer/iron-image/iron-image.js';
 import '@juanbeato/jb-button';
+import {defineCustomElements as initSkeleton} from  'skeleton-webcomponent-loader/loader/index.js';
 import style from './JbProductCard.scss';
 
 export class JbProductCard extends LitElement {
@@ -20,7 +22,8 @@ export class JbProductCard extends LitElement {
       view: { type: String },
       time: { type: String },
       price: { type: String },
-      buttonText: { type: String }
+      buttonText: { type: String },
+      isLoading: { type: Boolean }
     };
   }
 
@@ -33,19 +36,24 @@ export class JbProductCard extends LitElement {
     this.time = '';
     this.price = '';
     this.buttonText = '';
+    this.isLoading = true;
+    initSkeleton();
   }
 
   render() {
     return html`
-      <div class="jb-product-card__main-container">
+      <div class="jb-product-card__main-container"> 
         <div class="jb-product-card__main-container__image__container">
-          <img class="jb-product-card__main-container__image__container__img" src=${this.image}>
+          <img ?hidden=${this.isLoading} class="jb-product-card__main-container__image__container__img" 
+              src=${this.image}
+              @load=${() => this.isLoading = false}>
+          ${this._imageTemplateSkeleton}
         </div>
         <div class="jb-product-card__main-container__data-container">
-          ${this.constructor._paragraphTemplate(this.title, 'title')}
-          ${this.constructor._paragraphTemplate(this.description, 'description')}
+          ${this._paragraphTemplate(this.title, 'title')}
+          ${this._paragraphTemplate(this.description, 'description')}
           <div class="jb-product-card__main-container__data-container__footer">
-            ${this.constructor._timeTemplate(this.time)}
+            ${this._timeTemplate(this.time)}
             ${this._priceTemplate(this.price)}
           </div>
         </div>
@@ -53,28 +61,51 @@ export class JbProductCard extends LitElement {
     `;
   }
 
-  static _paragraphTemplate(text, type) {
+  get _imageTemplateSkeleton() {
+    return this.isLoading 
+      ? html`${ this.view === 'horizontal' 
+        ? unsafeHTML(`<nb-skeleton width="100%" height="100%"></nb-skeleton>`)
+        : unsafeHTML(`<nb-skeleton width="100%" height="100%"></nb-skeleton>`)}` 
+      : html``
+  }
+
+  _paragraphTemplate(text, type) {
     return text
       ? html`
-        <p class="jb-product-card__main-container__data-container__${type}">
-            ${text}
-        </p>`
+        <div class="jb-product-card__main-container__data-container__${type}">
+          ${ this.isLoading 
+            ? (type === 'description' ? unsafeHTML('<nb-skeleton count="3"></nb-skeleton>') : unsafeHTML('<nb-skeleton width="50%"></nb-skeleton>'))
+            : unsafeHTML(`<p class="jb-product-card__main-container__data-container__${type}__text">
+                  ${text}
+              </p>`)
+          }
+        </div>`
       : html``
   }
 
   _priceTemplate(price) {
-    return price
-      ? html`<p class="jb-product-card__main-container__data-container__footer__right">${price}</p>`
-      : html`<jb-button class="jb-product-card__main-container__data-container__footer__right"
-        secondary
-        buttontext=${this.buttonText}></jb-button>`;
+    return html`
+      <div class="jb-product-card__main-container__data-container__footer__right">
+      ${ this.isLoading 
+        ? unsafeHTML(`<nb-skeleton width="50%"></nb-skeleton>`)
+        : (price
+          ? unsafeHTML(`<p class="jb-product-card__main-container__data-container__footer__right__text">${price}</p>`)
+          : unsafeHTML(`<jb-button class="jb-product-card__main-container__data-container__footer__right__button"
+            secondary
+            buttontext=${this.buttonText}></jb-button>`))}
+      </div>`
   }
 
-  static _timeTemplate(time) {
+  _timeTemplate(time) {
     return time
-      ? html`<p class="jb-product-card__main-container__data-container__footer__left">
-        ${time}
-      </p>`
+      ? html`
+      <div class="jb-product-card__main-container__data-container__footer__left">
+        ${ this.isLoading 
+          ? unsafeHTML('<nb-skeleton width="50%"></nb-skeleton>')
+          : unsafeHTML(`<p class="jb-product-card__main-container__data-container__footer__left__text">
+              ${time}
+            </p>`)}
+      </div>`
       : html``
   }
 }
