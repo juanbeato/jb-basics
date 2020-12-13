0ceb287b44085d644db6428594ec3a03725dbc71
Juan Antonio Beato <juan.beato44@gmail.com>
2020-12-13 10:09:04 +0100

    feat(jb-product-card-list): added skeleton config to list

--- packages/jb-product-card-list/src/JbProductCardList.js
+++ packages/jb-product-card-list/src/JbProductCardList.js
@@ -16,6 +16,7 @@ export class JbProductCardList extends LitElement {
     return {
       cards: { type: Array },
       itemView: { type: String },
+      skeletonConfig: { type: Object },
     };
   }
 
@@ -23,25 +24,38 @@ export class JbProductCardList extends LitElement {
     super();
     this.cards = [];
     this.itemView = '';
+    this.skeletonConfig = {
+      items: 3,
+      itemConfig: {
+        image: '',
+        title: 'skeletonTitle',
+        price: 'skeletonPrice',
+      },
+    };
   }
 
   render() {
     return html`
       <div class="jb-product-card-list-container">
-        ${this.cards.map(
-          card =>
-            html`${unsafeHTML(`<jb-product-card class="jb-product-card-list-container__item"
-                  ${card.image ? `image="${card.image}"` : ''}
-                  ${card.title ? `title="${card.title}"` : ''}
-                  ${card.description ? `description="${card.description}"` : ''}
-                  view=${this.itemView}
-                  ${card.price ? `price="${card.price}"` : ''}
-                  ${card.time ? `time="${card.time}"` : ''}
-                  ${
-                    card.buttonText ? `buttontext="${card.buttonText}"` : ''
-                  }></jb-product-card>`)}`
-        )}
+        ${this.cards && this.cards.length
+          ? this.cards.map(card => this._productCardTemplate(card))
+          : [...Array(this.skeletonConfig.items)].map(() =>
+              this._productCardTemplate(this.skeletonConfig.itemConfig, true)
+            )}
       </div>
     `;
+  }
+
+  _productCardTemplate(card, mockCard) {
+    return html`${unsafeHTML(`<jb-product-card class="jb-product-card-list-container__item"
+        ${card.image ? `image="${!mockCard ? card.image : ''}"` : ''}
+        ${card.title ? `title="${card.title}"` : ''}
+        ${card.description ? `description="${card.description}"` : ''}
+        view=${this.itemView}
+        ${card.price ? `price="${card.price}"` : ''}
+        ${card.time ? `time="${card.time}"` : ''}
+        ${
+          card.buttonText ? `buttontext="${card.buttonText}"` : ''
+        }></jb-product-card>`)}`;
   }
 }
