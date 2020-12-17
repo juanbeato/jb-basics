04e9f49af6375c0d416abd3cb059a81f785e1d19
Juan Antonio Beato <juan.beato44@gmail.com>
2020-12-17 11:40:56 +0100

    feat(jb-product-card-list component): added subtitle and banner view custom properties

--- packages/jb-product-card-list/src/JbProductCardList.js
+++ packages/jb-product-card-list/src/JbProductCardList.js
@@ -50,6 +50,7 @@ export class JbProductCardList extends LitElement {
     return html`${unsafeHTML(`<jb-product-card class="jb-product-card-list-container__item"
         ${card.image ? `image="${!mockCard ? card.image : ''}"` : ''}
         ${card.title ? `title="${card.title}"` : ''}
+        ${card.subTitle ? `subtitle="${card.subTitle}"` : ''}
         ${card.description ? `description="${card.description}"` : ''}
         view=${this.itemView}
         ${card.price ? `price="${card.price}"` : ''}
