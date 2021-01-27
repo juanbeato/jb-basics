5984c402296919eeb493d73a28004d83262f464c
Juan Antonio Beato <juan.beato44@gmail.com>
2021-01-27 11:44:23 +0100

    feat(jb-modal): added id and config from open method

--- packages/jb-modal/src/JbModal.js
+++ packages/jb-modal/src/JbModal.js
@@ -1,58 +1,77 @@
 import { html, css, LitElement, unsafeCSS } from 'lit-element';
-import {ifDefined} from 'lit-html/directives/if-defined';
 import '@juanbeato/jb-button';
 import style from './JbModal.scss';
 
 export class JbModal extends LitElement {
   static get styles() {
-    return [css`${unsafeCSS(style)}`];
+    return [
+      css`
+        ${unsafeCSS(style)}
+      `,
+    ];
   }
 
   static get properties() {
     return {
       config: { type: Object },
-      opened: { 
+      opened: {
         type: Boolean,
-        reflect: true
-      }
+        reflect: true,
+      },
     };
   }
 
   constructor() {
     super();
-    this.title = 'Hey there';
     this.config = {};
   }
 
   render() {
     return html`
       <div class="jb-modal-container">
-        <div class="jb-modal-container__bg"
-        @click=${this.close}></div>
+        <div
+          class="jb-modal-container__bg"
+          @click=${this.close}
+          @keydown=${this.close}
+        ></div>
         <div class="jb-modal-container__data">
           <p class="jb-modal-container__data__title">${this.config.title}</p>
-          <p class="jb-modal-container__data__description">${this.config.description}</p>
+          <p class="jb-modal-container__data__description">
+            ${this.config.description}
+          </p>
           <div class="jb-modal-container__data__buttons">
-            ${this._buttonTemplate(this.config.acceptBtnText, 'primary')}
-            ${this._buttonTemplate(this.config.cancelBtnText, 'secondary')}
+            ${this._buttonTemplate(
+              this.config.acceptBtnText,
+              this.config.id,
+              'primary'
+            )}
+            ${this._buttonTemplate(
+              this.config.cancelBtnText,
+              this.config.id,
+              'secondary'
+            )}
           </div>
         </div>
       </div>
     `;
   }
 
-  _buttonTemplate(text, type) {
-    return text 
+  _buttonTemplate(text, id, type) {
+    return text
       ? html`<jb-button
-        class="jb-modal-container__data__buttons__item"
-        ?primary=${type === 'primary'}
-        ?secondary=${type === 'secondary'}
-        buttontext=${text}
-        @click=${() => this._handleClick(type)}></jb-button>`
-      : html``
+          class="jb-modal-container__data__buttons__item"
+          ?primary=${type === 'primary'}
+          ?secondary=${type === 'secondary'}
+          buttontext=${text}
+          @click=${() => this._handleClick(id, type)}
+        ></jb-button>`
+      : html``;
   }
 
-  open() {
+  open(config = {}) {
+    if (Object.keys(config).length) {
+      this.config = config;
+    }
     this.opened = true;
   }
 
@@ -60,11 +79,11 @@ export class JbModal extends LitElement {
     this.opened = false;
   }
 
-  _handleClick(type) {
+  _handleClick(id, type) {
     this.dispatchEvent(
-      new CustomEvent(`jb-modal-${type}-btn-clicked`, {
+      new CustomEvent(`jb-modal-${id}-${type}-btn-clicked`, {
         bubbles: true,
-        composed: true
+        composed: true,
       })
     );
     this.close();
