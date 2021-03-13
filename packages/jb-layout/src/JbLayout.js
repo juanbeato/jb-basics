07318e8ca7c97e51762f4d9662bc73490557c4f4
Juan Antonio Beato <juan.beato44@gmail.com>
2021-03-13 20:10:32 +0100

    feat(jb-layout): added header section

--- packages/jb-layout/src/JbLayout.js
+++ packages/jb-layout/src/JbLayout.js
@@ -1,4 +1,9 @@
 import { html, css, LitElement, unsafeCSS } from 'lit-element';
+import '@polymer/app-layout/app-drawer-layout/app-drawer-layout.js';
+import '@polymer/app-layout/app-header-layout/app-header-layout.js';
+import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
+import '@polymer/app-layout/app-header/app-header.js';
+import '@polymer/app-layout/app-toolbar/app-toolbar.js';
 import style from './JbLayout.scss';
 
 export class JbLayout extends LitElement {
@@ -17,12 +22,39 @@ export class JbLayout extends LitElement {
   render() {
     return html`
       <div class="jb-layout">
-        <div class="jb-layout__main">
-          <slot name="main"></slot>
-        </div>
-        <div class="jb-layout__footer">
-          <slot name="footer"></slot>
-        </div>
+        <app-drawer-layout>
+          <app-header-layout>
+            <!-- <app-header class="jb-layout__header" 
+              condenses 
+              fixed 
+              shadow 
+              effects="blend-background resize-title waterfall" slot="header">
+              <app-toolbar>
+                <div condensed-title>
+                  <div class="jb-layout__header__condensed-title">
+                    <slot name="header-condensed-title">
+                    </slot>
+                  </div>
+                </div>
+              </app-toolbar>
+              <app-toolbar>
+                <div main-title>
+                  <div class="jb-layout__header__main-title">
+                    <slot name="header-main-title">
+                    </slot>
+                  </div>
+                </div>
+              </app-toolbar>
+            </app-header> -->
+            <div class="jb-layout__main">
+              <slot name="main">
+              </slot>
+            </div>
+            <div class="jb-layout__footer">
+              <slot name="footer"></slot>
+            </div>
+          </app-header-layout>
+        </app-drawer-layout>
       </div>
     `;
   }
